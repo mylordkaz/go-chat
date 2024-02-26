8ec9256fc14550b5459d98f104950e75f379702a
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-26 15:49:05 +0900

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// server setup
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
+
+func main(){
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
