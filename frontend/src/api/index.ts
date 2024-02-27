697b9da113bd8a7fd259228030f3264b2aa90eb8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-27 17:44:24 +0900

    chore: Ts style update

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,35 +1,30 @@
-let socket = new WebSocket("ws://localhost:8080/ws")
+const socket = new WebSocket("ws://localhost:8080/ws")
 
 
-// connect to the WebSocket endpoint and listens for events
-export const connect = () =>{
-    console.log("Attempting Connection...")
-    
+const connect = (cb: (msg: MessageEvent) => void): void => {
+    console.log("connecting");
+  
     socket.onopen = () => {
-        console.log("Successfully Connected")
-    }
-
+      console.log("Successfully Connected");
+    };
+  
     socket.onmessage = (msg) => {
-        
-        console.log(msg)
-    }
-
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event)
-    }
-
-    socket.onerror = error => {
-        console.log("Socket Error: ", error)
-    }
-}
-
-//allows to send msg, frontend >>> backend 
-export const sendMsg = (msg: string) => {
-    console.log("sending msg:", msg)
-    socket.send(msg)
-}
-// export const sendMsg = (username: string, message: string) => {
-//     const payload = JSON.stringify({ username, message });
-//     console.log("sending msg:", payload);
-//     socket.send(payload);
-//   };
+      console.log("Message from WebSocket: ", msg);
+      cb(msg);
+    };
+  
+    socket.onclose = (event) => {
+      console.log("Socket Closed Connection: ", event);
+    };
+  
+    socket.onerror = (error) => {
+      console.log("Socket Error: ", error);
+    };
+  };
+  
+const sendMsg = (msg: string): void => {
+    console.log("sending msg: ", msg);
+    socket.send(msg);
+};
+      
+export { connect, sendMsg };
